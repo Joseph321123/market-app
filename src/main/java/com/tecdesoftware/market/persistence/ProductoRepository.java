e41e7359fd87f2b7fecd14802c1eba6d6c1b0ce7
Joseph321123 <aguilarrodriguez1.15@gmail.com>
2025-06-16 20:58:17 -0600

    Correccion Crud repository and persistence dependencies

--- src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
+++ src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
@@ -1,13 +1,21 @@
 package com.tecdesoftware.market.persistence;
 
-package persistence;
+import com.tecdesoftware.market.persistence.crud.ProductoCrudRepository;
+import com.tecdesoftware.market.persistence.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public class ProductoRepository {
-    private ProductosCrudRepository productosCrudRepository;
 
-    //Deberia de ser solo List<Producto>
-    public List<com.tecdesoftware.market.persistence.entity.Producto> findAll() {
-        return (List<com.tecdesoftware.market.persistence.entity.Producto>) productos;
+    @Autowired
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    // Puedes agregar más métodos como save, delete, etc.
 }
